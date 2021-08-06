663ac58ae756c508f98a3811fe9d3fe5a82ba41d
Poonai <rbalajis25@gmail.com>
2021-08-06 23:59:20 +0530

    marketplace link added
    Signed-off-by: Poonai <rbalajis25@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="https://marketplace.visualstudio.com/items?itemName=quicklog.quicklog">
             View on Marketplace 🏭
           </Link>
         </div>
